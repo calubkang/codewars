adfaa3b9627fdda8b41b0a05fa826653cf98dc73
calubkang <clbkang@gmail.com>
2021-11-01 15:20:36 -0700

    solutions

---
+++ js/findStrayNum.js
@@ -0,0 +1,8 @@
+function stray(numbers) {
+    numbers.sort((a, b) => a - b);
+    if (numbers[0] === numbers[1]) {
+        return numbers[numbers.length - 1]
+    } else {
+        return numbers[0]
+    }
+}
