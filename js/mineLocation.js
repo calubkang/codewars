1b1e6d5eabb6ab58b8f4a510dbd99be9268ff073
calubkang <clbkang@gmail.com>
2022-03-23 17:03:59 -0700

    solutions

--- js/mineLocation.js
+++ js/mineLocation.js
@@ -1,14 +1,14 @@
-function mineLocation(field){
-  let estimate, exact;
-  field.forEach((chunk, i)=>{
-    if(chunk.includes(1)){
-      estimate = i
-    }
-  })
-  field[estimate].forEach((attempt, i)=>{
-    if(attempt === 1){
-      exact = i
-    }
-  })
-  return [estimate, exact]
+function mineLocation(field) {
+    let estimate, exact;
+    field.forEach((chunk, i) => {
+        if (chunk.includes(1)) {
+            estimate = i
+        }
+    })
+    field[estimate].forEach((attempt, i) => {
+        if (attempt === 1) {
+            exact = i
+        }
+    })
+    return [estimate, exact]
 }
---
+++ js/stringArrayDuplicates.js
@@ -0,0 +1,5 @@
+function dup(s) {
+    return s.map(word => {
+        return word.split('').filter((char, i, arr) => char !== arr[i + 1]).join('')
+    })
+};
