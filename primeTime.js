787210c8df4bb61b8f9748fedf03ed44c81a2f3b
calubkang <clbkang@gmail.com>
2022-05-10 21:37:39 -0700

    solutions

--- primeTime.js
+++ primeTime.js
@@ -1,23 +1,23 @@
 function prime(num) {
-  let res = [];
-  for(let i=2; i<=num; i++){
-    if(isPrime(i)){
-      res.push(i)
+    let res = [];
+    for (let i = 2; i <= num; i++) {
+        if (isPrime(i)) {
+            res.push(i)
+        }
     }
-  }
-  return res
+    return res
 }
 
-function isPrime(num){
-  
-  if(num<2){
-    return false
-  }
-  
-  for(let i=2; i<=Math.sqrt(num);i++){
-    if(num%i===0){
-      return false
+function isPrime(num) {
+
+    if (num < 2) {
+        return false
     }
-  }
-  return true
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false
+        }
+    }
+    return true
 }
