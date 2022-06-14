b96231cfc9db146572433dfa17dac8f72ed8745d
calubkang <clbkang@gmail.com>
2022-06-14 10:33:46 -0700

    solution

--- js/mostMoneyOOP.js
+++ js/mostMoneyOOP.js
@@ -1,15 +1,15 @@
-Student.prototype.total = function() {
-  return this.fives * 5 + this.tens * 10 + this.twenties * 20;
+Student.prototype.total = function () {
+    return this.fives * 5 + this.tens * 10 + this.twenties * 20;
 }
 
 function mostMoney(students) {
-  if (students.length === 1) return students[0].name;
-  
-  let totals = new Set();
-  students.forEach(student => {
-    totals.add(student.total())
-  })
-  
-  return totals.size === 1 ? 'all' : students.sort((a, b) => b.total() - a.total())[0].name
+    if (students.length === 1) return students[0].name;
+
+    let totals = new Set();
+    students.forEach(student => {
+        totals.add(student.total())
+    })
+
+    return totals.size === 1 ? 'all' : students.sort((a, b) => b.total() - a.total())[0].name
 }
 
