a90967b048d8144c75bafd4dddf9b7456867a7ba
calubkang <clbkang@gmail.com>
2022-06-29 11:39:04 -0700

    solution

--- js/splitAndAdd.js
+++ js/splitAndAdd.js
@@ -1,23 +1,23 @@
 function splitAndAdd(arr, n) {
-  while (n > 0) {
-    if (arr.length % 2) {
-      let res = [];
-      res.push(arr[Math.floor(arr.length/2)])
-      for (let i=0; i<(arr.length - 1)/2; i++){
-        res.push(arr[i] + arr[Math.ceil(arr.length/2) + i])
-      }
-      arr = res;
-      n--
+    while (n > 0) {
+        if (arr.length % 2) {
+            let res = [];
+            res.push(arr[Math.floor(arr.length / 2)])
+            for (let i = 0; i < (arr.length - 1) / 2; i++) {
+                res.push(arr[i] + arr[Math.ceil(arr.length / 2) + i])
+            }
+            arr = res;
+            n--
+        }
+
+        else {
+            let res = [];
+            for (let i = 0; i < arr.length / 2; i++) {
+                res.push(arr[i] + arr[arr.length / 2 + i])
+            }
+            arr = res;
+            n--
+        }
     }
-    
-    else {
-      let res = [];
-      for (let i=0; i<arr.length/2; i++){
-        res.push(arr[i] + arr[arr.length/2 + i])
-      }
-      arr = res;
-      n--
-    }
-  }
-  return arr
+    return arr
 }
