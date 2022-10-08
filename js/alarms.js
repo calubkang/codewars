241a6cbd5d9631ff5bbc66ee2ef9995e246f4a3c
calubkang <clbkang@gmail.com>
2022-10-07 22:47:17 -0700

    new solution

--- js/alarms.js
+++ js/alarms.js
@@ -1,18 +1,18 @@
-function solve(arr){
-  
+function solve(arr) {
+
   // case for only one alarm
-  if(arr.length === 1){
+  if (arr.length === 1) {
     return '23:59'
   }
   let intervals = arr.sort().map((t, i, times) => {
     // disregard all duplicates
-    if(t === times[i+1]){
+    if (t === times[i + 1]) {
       return '0'
     }
-    
+
     // allowing for the interval to be calculated between today's last and tomorrow's first alarm
-    let nextIndex = i < times.length - 1 ? i+1 : 0;
-    
+    let nextIndex = i < times.length - 1 ? i + 1 : 0;
+
     let [nextHH, nextMM] = times[nextIndex].split(':').map(t => Number(t));
     let [curHH, curMM] = t.split(':').map(t => Number(t));
     let passedMM = nextMM - curMM - 1;
@@ -24,10 +24,10 @@ function solve(arr){
     if (passedHH < 0) {
       passedHH += 24
     }
-    if(passedMM < 10){
+    if (passedMM < 10) {
       passedMM = `0${passedMM}`
     }
-    if(passedHH < 10){
+    if (passedHH < 10) {
       passedHH = `0${passedHH}`
     }
     return `${passedHH}:${passedMM}`
