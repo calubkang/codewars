00aa094023bc2eeb2e3370bbfb91a6042ca47ffe
calubkang <clbkang@gmail.com>
2022-10-14 13:06:44 -0700

    new solution

--- js/sameTree.js
+++ js/sameTree.js
@@ -1,25 +1,7 @@
 var isSameTree = function (p, q) {
-  if (!p ^ !q) {
-    return false
-  } else if (!p && !q) {
-    return true
-  }
-  let [pq, qq] = [[p], [q]];
-  while (pq.length && qq.length) {
-    const [pLength, qLength] = [pq.length, qq.length]
-    if (pLength != qLength) {
-      return false
-    }
-    for (let i = 0; i < pLength; i++) {
-      let [pCur, qCur] = [pq.pop(), qq.pop()];
-      if ((pCur.val != qCur.val) || (!pCur.left ^ !qCur.left) || (!pCur.right ^ !qCur.right)) {
-        return false
-      }
-      pCur.left && pq.unshift(pCur.left)
-      qCur.left && qq.unshift(qCur.left)
-      pCur.right && pq.unshift(pCur.right)
-      qCur.right && qq.unshift(qCur.right)
-    }
-  }
-  return true
+  if (!p && !q) return true
+  if (!p || !q) return false
+  if (p.val !== q.val) return false
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
 };
