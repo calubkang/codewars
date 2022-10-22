aab9e1506f89646a4540fccd993ad012a6325905
calubkang <clbkang@gmail.com>
2022-10-22 13:54:06 -0700

    new solution

---
+++ js/alternateIndexInList.js
@@ -0,0 +1,16 @@
+var oddEvenList = function (head) {
+  if (!head) {
+    return head
+  }
+  let odd = head;
+  let even = head.next;
+  const evenHead = even;
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+  odd.next = evenHead;
+  return head
+};
